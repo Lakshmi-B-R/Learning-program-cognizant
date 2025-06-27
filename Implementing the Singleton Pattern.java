be114d224e1b9ac89be9f0e4a4e5e53e68672e16
Lakshmi-B-R <lakshmibr2512@gmail.com>
2025-06-27 12:01:44 +0530

    Update Implementing the Singleton Pattern.java

--- Implementing the Singleton Pattern.java
+++ Implementing the Singleton Pattern.java
@@ -2,7 +2,7 @@ public class Singleton {
     private static Singleton instance;
 
     private Singleton() {
-        // private constructor
+        //private constructor is created
     }
 
     public static Singleton getInstance() {
