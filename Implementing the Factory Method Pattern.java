51a0ea30dd3f955bea36f1f2c5fd3cb01451f00a
Lakshmi-B-R <lakshmibr2512@gmail.com>
2025-06-27 12:04:04 +0530

    Add files via upload

---
+++ Implementing the Factory Method Pattern.java
@@ -0,0 +1,26 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Square implements Shape {
+    public void draw() {
+        System.out.println("Drawing Square");
+    }
+}
+
+class ShapeFactory {
+    public Shape getShape(String shapeType) {
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        return null;
+    }
+}
